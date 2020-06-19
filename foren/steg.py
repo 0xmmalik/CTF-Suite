7ce71787f484bca7e88d638afaebd4525af6db7f
Manav Malik <manavmalik36@gmail.com>
2020-06-19 15:35:13 -0400

    added lsb steganography tools to foren

---
+++ foren/steg.py
@@ -0,0 +1,43 @@
+###########################
+### IMAGE STEGANOGRAPHY ###
+###########################
+from PIL import Image
+
+
+def getPixels(filepath):
+    return Image.open(filepath).load()
+
+
+def bits2string(b=None):
+    return ''.join([chr(int(x, 2)) for x in b])
+
+
+def getLsb(filepath):
+    extracted_bin = []
+    with Image.open(filepath) as img:
+        width, height = img.size
+        byte = []
+        for x in range(0, width):
+            for y in range(0, height):
+                pixel = list(img.getpixel((x, y)))
+                for n in range(0, 3):
+                    extracted_bin.append(pixel[n] & 1)
+
+    extracted_bin = "".join([str(x) for x in extracted_bin])
+    return ''.join(chr(int(extracted_bin[i:i + 8], 2)) for i in range(0, len(extracted_bin), 8))
+
+
+def encLsb(filepath, message, outputfile):
+    bin_message = ''.join('{:08b}'.format(ord(c)) for c in message)
+    i = 0
+    with Image.open(filepath) as img:
+        width, height = img.size
+        for x in range(0, width):
+            for y in range(0, height):
+                pixel = list(img.getpixel((x, y)))
+                for n in range(0, 3):
+                    if i < len(bin_message):
+                        pixel[n] = pixel[n] & ~1 | int(bin_message[i])
+                        i += 1
+                img.putpixel((x, y), tuple(pixel))
+        img.save(outputfile, "PNG")
