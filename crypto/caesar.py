da6196405051d5a1cebf76bbf288795e36cbe75f
Manav Malik <manavmalik36@gmail.com>
2020-06-19 15:32:02 -0400

    modified caesar and vigenere ciphers in crypto

--- crypto/caesar.py
+++ crypto/caesar.py
@@ -2,17 +2,27 @@
 ### CAESAR CIPHER ###
 #####################
 
+CAESAR = "caesar"
+ROT13 = "ROT13"
+ROT18 = "ROT18"
+
+
 def encode(plaintext, key=None, variant="caesar"):
     ciphertext = ""
     if "ROT" in variant: key = 13
     if key == None: raise Exception("You must enter a key for the caesar cipher variant!")
     for i in range(len(plaintext)):
         char = plaintext[i]
-        if char.isupper(): ciphertext += chr((ord(char) + key - 65) % 26 + 65)
-        elif char.islower(): ciphertext += chr((ord(char) + key - 97) % 26 + 97)
-        elif char.isdigit() and variant == "ROT18": ciphertext += str((int(char) + 5) % 10)
-        else: ciphertext += char
+        if char.isupper():
+            ciphertext += chr((ord(char) + key - 65) % 26 + 65)
+        elif char.islower():
+            ciphertext += chr((ord(char) + key - 97) % 26 + 97)
+        elif char.isdigit() and variant == "ROT18":
+            ciphertext += str((int(char) + 5) % 10)
+        else:
+            ciphertext += char
     return ciphertext
+
 
 def decode(ciphertext, key=None, search="", variant="caesar"):
     if "ROT" in variant: return encode(ciphertext, variant=variant)
@@ -22,5 +32,4 @@ def decode(ciphertext, key=None, search="", variant="caesar"):
             p = encode(ciphertext, -i)
             if search in p: plaintexts.append(p)
         return plaintexts
-    return encode(ciphertext, -key)
-
+    return encode(ciphertext, -key), key
--- crypto/vigenere.py
+++ crypto/vigenere.py
@@ -4,8 +4,10 @@
 
 import itertools
 
-ENG_FREQ = [0.0749, 0.0129, 0.0354, 0.0362, 0.1400, 0.0218, 0.0174, 0.0422, 0.0665, 0.0027, 0.0047, 0.0357, 0.0339, 0.0674, 0.0737, 0.0243, 0.0026, 0.0614, 0.0695, 0.0985, 0.0300, 0.0116, 0.0169, 0.0028, 0.0164, 0.0004]
+ENG_FREQ = [0.0749, 0.0129, 0.0354, 0.0362, 0.1400, 0.0218, 0.0174, 0.0422, 0.0665, 0.0027, 0.0047, 0.0357, 0.0339,
+            0.0674, 0.0737, 0.0243, 0.0026, 0.0614, 0.0695, 0.0985, 0.0300, 0.0116, 0.0169, 0.0028, 0.0164, 0.0004]
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
 
 def encode(plaintext, key):
     ciphertext = ""
@@ -19,8 +21,10 @@ def encode(plaintext, key):
         elif char.islower():
             ciphertext += chr((ord(char) + ord(keychar) - 97 - 97) % 26 + 97)
             x += 1
-        else: ciphertext += char
+        else:
+            ciphertext += char
     return ciphertext
+
 
 def freq_diff(text):
     text = [t for t in text.lower() if t.islower()]
@@ -28,6 +32,7 @@ def freq_diff(text):
     total = float(len(text))
     for let in text: freq[ord(let) - ord('a')] += 1
     return sum(abs(f / total - E) for f, E in zip(freq, ENG_FREQ))
+
 
 def decode_with_key(ciphertext, key):
     plaintext = ""
@@ -41,21 +46,26 @@ def decode_with_key(ciphertext, key):
         elif char.islower():
             plaintext += chr((ord(char) - ord(keychar) + 97 - 97) % 26 + 97)
             x += 1
-        else: plaintext += char
+        else:
+            plaintext += char
     return plaintext
+
 
 def decode(ciphertext, key=None, minkeysize=3, maxkeysize=25):
     if key == None:
-        poss = []
-        letters = [let for let in ciphertext if let.islower() or let.isupper()]
-        for keylen in range(minkeysize, maxkeysize):
-            k = [None] * keylen
-            for i in range(keylen):
-                mod_letters = "".join(itertools.islice(letters, i, None, keylen))
-                shifts = []
-                for let in ALPHABET: shifts.append((freq_diff(decode_with_key(mod_letters, let)), let))
-                k[i] = min(shifts, key=lambda x: x[0])[1]
-            poss.append("".join(k))
-        poss.sort(key=lambda key: freq_diff(decode_with_key(ciphertext, key)))
-        key = poss[0]
-    return decode_with_key(ciphertext, key)
+        try:
+            poss = []
+            letters = [let for let in ciphertext if let.islower() or let.isupper()]
+            for keylen in range(minkeysize, maxkeysize):
+                k = [None] * keylen
+                for i in range(keylen):
+                    mod_letters = "".join(itertools.islice(letters, i, None, keylen))
+                    shifts = []
+                    for let in ALPHABET: shifts.append((freq_diff(decode_with_key(mod_letters, let)), let))
+                    k[i] = min(shifts, key=lambda x: x[0])[1]
+                poss.append("".join(k))
+            poss.sort(key=lambda key: freq_diff(decode_with_key(ciphertext, key)))
+            key = poss[0]
+        except:
+            raise Exception("Key could not be deduced")
+    return decode_with_key(ciphertext, key), key
