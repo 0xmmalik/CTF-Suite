468999c11fb895d2dded0be7a7eb7422da8db1bc
Manav Malik <manavmalik36@gmail.com>
2020-06-21 00:05:19 -0400

    added image steg tests to forensics

--- forentest.py
+++ forentest.py
@@ -2,10 +2,16 @@
 ### FORENSICS TEST ###
 ######################
 
-from foren import audsteg
+from foren import audsteg, imgsteg
 
 a = audsteg.get_lsb("example.wav")
 print("Original Audio LSBs: " + a[:100] + "... (truncated)")
 audsteg.enc_lsb("example.wav", "Hi there! I love audio steganography. Don't you?", "example_encoded.wav")
 a = audsteg.get_lsb("example_encoded.wav")
 print("Encoded Audio LSBs: " + a[:100] + "... (truncated)")
+
+i = imgsteg.get_lsb("example.png")
+print("Original Image LSBs: " + i[:100] + "... (truncated)")
+imgsteg.encode_lsb("example.png", "Magic is real. This is a fact!", "example_encoded.png")
+i = imgsteg.get_lsb("example_encoded.png")
+print("Encode Image LSBs: " + i[:100] + "... (truncated)")
